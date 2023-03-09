049abde28ab2e0df9743a01dcb63bdce3e3e8732
Dominiq Jakab <dominiq50@gmail.com>
2023-03-09 17:36:32 +0200

    Description added

--- Aplicatii/Descarcare_pagina_web.py
+++ Aplicatii/Descarcare_pagina_web.py
@@ -1,18 +1,23 @@
 #!/usr/bin/pythonn3
 
+### Algorithm used to scrape a web page in a html file - version 1
+### apt-get install urllib2-file urllib3
+
+### Part 0 ### Import necessary modules
 import codecs
 from urllib import request, response, error, parse
 
+### Part 1 ### Define the url and request from the server
 url = "https://google.ro"
 # print(dir(request))
-raspuns_server = request.urlopen(url)
-date = raspuns_server.read()
+reply_server = request.urlopen(url)
+data = reply_server.read()
 
 try:
     fisier_html = open("/home/dj/PycharmProjects/Programare_Python/Aplicatii/site_downloadat.html", "w")
-    fisier_html.write(str(date))
+    fisier_html.write(str(data))
 except Exception:
-    print("Procesul nu s-a incheiat cu succes.")
+    print("The process finished unsuccesfully.")
 else:
     fisier_html.close()
 
