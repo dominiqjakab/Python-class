d2f2b5f1c59baadad781250c200f2953c517fc00
Dominiq Jakab <dominiq50@gmail.com>
2023-03-09 17:49:49 +0200

    Description and language updated

--- Aplicatii/Mail_sender.py
+++ Aplicatii/Mail_sender.py
@@ -1,32 +1,34 @@
 #!/usr/bin/python3
 
+### Algorithm used to send emails using SMTP server ###
+### apt-get mime
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-def sendMail(fromAdd, toAdd, subiect, mesaj):
-    ### Part 0 ### Crearea email-ului in formatul MIME
+def sendMail(fromAdd, toAdd, subject, message):
+    ### Part 0 ### Creating the email in MIME format
 
     msg = MIMEMultipart()
     msg["From"] = fromADD
     msg["To"] = toADD
-    msg["Subject"] = subiect
+    msg["Subject"] = subject
     print("Creating the message")
 
-    ### Part 1 ### Crearea mesajului
+    ### Part 1 ### Creating the message
 
-    msg.attach(MIMEText(mesaj,'plain'))
+    msg.attach(MIMEText(message,'plain'))
 
-    ### Part 2 ### Conectarea la server
+    ### Part 2 ### Connecting to the server
 
     server = smtplib.SMTP("smtp.gmail.com", 587)
     server.starttls()
-    # Logarea cu credentialele pentru aplicatie din contul de gmail
-    server.login(fromADD, "token cont gmail")
+    # Login with the credentials used for the gmail account
+    server.login(fromADD, "token app from the google account ")
 
     print("Connecting to server")
 
-    ### Part 3 ###
+    ### Part 3 ### Sending the email
 
     server.sendmail(fromADD,toADD,msg.as_string())
 
@@ -34,8 +36,8 @@ def sendMail(fromAdd, toAdd, subiect, mesaj):
 
 fromADD = "testdj500@gmail.com"
 toADD = "dominiq50@gmail.com"
-subiect = "HeLLO FROM Python"
-mesaj = "Te salut! Din programul meu scris in Python"
+subject = "HeLLO FROM Python"
+message = "Hello! From my algorithm written in Python"
 
 if True:
-    sendMail(fromADD, toADD, subiect, mesaj)
+    sendMail(fromADD, toADD, subject, message)
